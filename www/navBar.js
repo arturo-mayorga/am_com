1f3c1f6c3a98df3bfede81b110f2b81c773f77ca
arturo-mayorga <arturo.mayorga@gmail.com>
2015-07-15 21:15:07 -0400

    Updating audio playback controls.

--- www/navBar.js
+++ www/navBar.js
@@ -3,6 +3,16 @@ function NavBar(domObj)
 {
     this._domObj = domObj;
     this._jQObj = $("#navBar");
+    this._playlist = [
+        'a0208.mp3',
+        'a0209.mp3',
+        'a0210.mp3'
+    ];
+
+    this._track = 0;
+
+    this._isPlaying = false;
+
     this.populate = function()
     {
         var navBarSideBtn = document.createElement('div');
@@ -19,30 +29,25 @@ function NavBar(domObj)
         
         var navBarPlayer = document.createElement('div');
         navBarPlayer.className = "navBarPlayer";
-		
-        var audio = document.createElement('audio');
-        audio.id = "barAudio";
-        var asrc = document.createElement('source');
-        asrc.type = 'audio/mpeg'; asrc.src = 'a0210.mp3';
-        audio.appendChild(asrc);
+        this._navBarPlayer = navBarPlayer;
 		
         var navBarRwBtn = document.createElement('div');
         navBarRwBtn.className = "navBarRwBtn";
-        navBarRwBtn.onclick = function() {audio.play();};
+        navBarRwBtn.onclick = function(e){e.stopPropagation(); this._playPrevTrack();}.bind(this);
+
         navBarPlayer.appendChild(navBarRwBtn);
         
         var navBarPlayBtn = document.createElement('div');
         navBarPlayBtn.className = "navBarPlayBtn";
-        navBarPlayBtn.onclick = function(){};
+        navBarPlayBtn.onclick = function(e){e.stopPropagation(); this._playTrack(this._track);}.bind(this);
         navBarPlayer.appendChild(navBarPlayBtn);
+        this._playPauseDom = navBarPlayBtn;
         
         var navBarFfBtn = document.createElement('div');
         navBarFfBtn.className = "navBarFfBtn";
-        navBarFfBtn.onclick = function(){};
+        navBarFfBtn.onclick = function(e){e.stopPropagation(); this._playNextTrack();}.bind(this);
         navBarPlayer.appendChild(navBarFfBtn);
-        
-        navBarPlayer.appendChild(audio);
-        
+
         this._domObj.appendChild(navBarPlayer);
     };
     
@@ -57,4 +62,52 @@ function NavBar(domObj)
     };
 
     this.distributeMessage = function(){};
+
+    this._pauseTrack = function()
+    {
+        if ( undefined !== this._audioDom )
+        {
+            this._audioDom.pause();
+        }
+
+        this._playPauseDom.className = 'navBarPlayBtn';
+        this._playPauseDom.onclick = function(e){e.stopPropagation(); this._playTrack(this._track);}.bind(this);
+    };
+
+    this._playNextTrack = function()
+    {
+        this._playTrack( ++this._track );
+    };
+
+    this._playPrevTrack = function()
+    {
+        this._track = this._track + this._playlist.length - 1;
+        this._playTrack( this._track );
+    };
+
+    this._playTrack = function( track )
+    {
+        this._track = track = track % this._playlist.length;
+
+        if ( undefined !== this._audioDom )
+        {
+            this._audioDom.pause();
+            this._navBarPlayer.removeChild( this._audioDom );
+            this._audioDom = undefined;
+        }
+
+        var audio = document.createElement('audio');
+        audio.id = "barAudio";
+        var asrc = document.createElement('source');
+        asrc.type = 'audio/mpeg'; asrc.src = this._playlist[track];
+        audio.appendChild(asrc);
+        this._navBarPlayer.appendChild(audio);
+
+        audio.play();
+
+        this._playPauseDom.className = 'navBarPauseBtn';
+        this._playPauseDom.onclick = function(e){e.stopPropagation(); this._pauseTrack();}.bind(this);
+
+        this._audioDom = audio;
+    };
 }
